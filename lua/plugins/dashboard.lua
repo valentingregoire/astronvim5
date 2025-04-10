d778b6604c7235dac36dcf2bc66a2711157fdd38
Valentin Grégoire <valentin.gregoire@gmail.com>
2025-04-10 13:30:41 +0200

    dashboard stuff

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -23,9 +23,7 @@ return {
  ███████████ ███    ███ █████████ █████ █████ ████ █████ 
 ██████  █████████████████████ ████ █████ █████ ████ ██████]],
         keys = {
-          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
           {
             icon = " ",
@@ -34,8 +32,13 @@ return {
             action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
           },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-          -- { icon = "󰅙 ", key = "q", desc = "Quit", action = ":qa" },
+          {
+            icon = "󰚰 ",
+            key = "U",
+            desc = "Check Updates",
+            action = ":Lazy check",
+            enabled = package.loaded.lazy ~= nil,
+          },
         },
       },
       sections = {
@@ -51,8 +54,21 @@ return {
           icon = " ",
           title = { "Projects", hl = "SnacksDashboardDesc" },
           indent = 2,
+          key = "p",
+          action = ":lua Snacks.dashboard.pick('projects')",
         },
-        { icon = "󰅙 ", key = "q", desc = "Quit", action = ":qa", padding = { 1, 0 } },
+        -- { icon = "󰅙 ", key = "q", desc = "Quit", action = ":qa", padding = { 1, 0 } },
+        {
+          section = "terminal",
+          height = 3,
+          cmd = "gh notify -sn 5; sleep 0.01",
+          title = { "GitHub Notifications", hl = "SnacksDashboardDesc" },
+          indent = 2,
+          icon = "󰊤 ",
+          key = "G",
+          action = function() vim.ui.open "https://github.com/notifications" end,
+        },
+        { icon = "󰅙 ", key = "q", desc = "Quit", action = ":qa", padding = { 2, 0 } },
         { section = "startup" },
       },
     },
