beb1f6f047e80141e311ca117fe7576e72400e1f
Valentin Grégoire <valentin.gregoire@gmail.com>
2026-06-26 21:07:38 +0200

    fixes
    Signed-off-by: Valentin Grégoire <valentin.gregoire@gmail.com>

--- lua/community.lua
+++ lua/community.lua
@@ -60,7 +60,7 @@ return {
   { import = "astrocommunity.pack.helm" },
   -- { import = "astrocommunity.pack.nginx" },
   -- markdown/latex
-  { import = "astrocommunity.pack.markdown" },
+  -- { import = "astrocommunity.pack.markdown" },
   -- { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
 
   -- running/debugging
--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,11 @@
 -- all options can be set here, as the options in astrocore.lua don't seem to be picked up
-vim.opt.spell = true
+vim.opt.spell = false
 vim.opt.spelloptions = "noplainbuffer,camel"
 vim.opt.spellfile = vim.fn.stdpath "config" .. "/spell/en.utf-8.add"
 -- vim.lsp.semantic_tokens = false
+
+vim.diagnostic.config {
+  float = {
+    source = true,
+  },
+}
